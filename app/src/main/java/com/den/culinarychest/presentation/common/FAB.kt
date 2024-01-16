3a6bc11259eec835ea7dba9d0eaad26314569104
Denis Frolkov <deniska-frolkov@mail.ru>
2024-01-16 17:19:13 +0300

    Implemented FAB

---
+++ app/src/main/java/com/den/culinarychest/presentation/common/FAB.kt
@@ -0,0 +1,33 @@
+package com.den.culinarychest.presentation.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.den.culinarychest.presentation.ui.theme.SoftOrange
+import com.den.culinarychest.R
+import com.den.culinarychest.presentation.ui.theme.SoftGray
+
+@Composable
+fun FAB() {
+    Box(
+        modifier = Modifier
+            .background(color = SoftOrange, shape = RoundedCornerShape(size = 25.dp))
+            .border(width = 0.5.dp, color = SoftGray, shape = RoundedCornerShape(size = 25.dp))
+            .size(size = 50.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.size(size = 20.dp),
+            painter = painterResource(id = R.drawable.add_icon),
+            contentDescription = "Тут должна быть иконка добавления"
+        )
+    }
+}
--- app/src/main/java/com/den/culinarychest/presentation/screens/CreatedScreen.kt
+++ app/src/main/java/com/den/culinarychest/presentation/screens/CreatedScreen.kt
@@ -1,9 +1,27 @@
 package com.den.culinarychest.presentation.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.den.culinarychest.presentation.common.FAB
+import com.den.culinarychest.presentation.ui.theme.SoftPink
 
 @Composable
-fun CreatedScreen(modifier: Modifier = Modifier) {
-
+fun CreatedScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = SoftPink))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(end = 16.dp, bottom = 66.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        FAB()
+    }
 }
